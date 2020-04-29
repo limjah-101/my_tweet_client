7b514e0ce8dc5d22b17e6a7754a14457a69b7e2e
liva.r <liv.ratsimbazafy@gmail.com>
2020-04-29 23:15:24 +0200

    [createTweet inputFile]-fixed

---
+++ build/precache-manifest.270f7fd73ee2fd2d362cefe1ddd983d3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78514f488da89e96c79d9abfbd5e6110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ff1fdeecdffdf71ec29",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c82d821581a4042706e4",
+    "url": "/static/css/main.61dfd9e6.chunk.css"
+  },
+  {
+    "revision": "6ff1fdeecdffdf71ec29",
+    "url": "/static/js/2.09394036.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.09394036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c82d821581a4042706e4",
+    "url": "/static/js/main.1ea38bb6.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "f817a688c63810ffdcf0ded4f9341a55",
+    "url": "/static/media/avatar.f817a688.png"
+  },
+  {
+    "revision": "fc6a747513a4c788fe23d91db0fdaeaf",
+    "url": "/static/media/default-avatar.fc6a7475.jpg"
+  },
+  {
+    "revision": "fed36d150a4217cea203fb2a71e2db7e",
+    "url": "/static/media/node.fed36d15.png"
+  },
+  {
+    "revision": "0a8f7727d33a1eca8b149a134c746666",
+    "url": "/static/media/sz-logo.0a8f7727.png"
+  },
+  {
+    "revision": "d84c35877baeb4ac8ea522bce7fc601b",
+    "url": "/static/media/tropical-escape.d84c3587.jpg"
+  },
+  {
+    "revision": "f6d83cc4ba7a6402f12fb77dc7d14279",
+    "url": "/static/media/unsplash.f6d83cc4.jpg"
+  },
+  {
+    "revision": "362d508a9e953da1777af9003b672358",
+    "url": "/static/media/wp.362d508a.jpg"
+  }
+]);
